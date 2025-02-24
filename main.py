739896595e155c688245a1af12c8ae3b5ec1c1c6
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-24 21:57:23 +0500

    coding challenge first

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-def formated_name(f_name, l_name):
-    formated_fname = f_name.title()
-    formated_lname = l_name.title()
-    return f"{formated_fname} {formated_lname}"
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        print("Leap year.")
+      else:
+        print("Not leap year.")
+    else:
+      print("Leap year.")
+  else:
+    print("Not leap year.")
+
+def days_in_month():
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
 
 
-print(formated_name("ArslAn", "Yousaf"))
+
+
+
+
+
+
+
+
+
+
