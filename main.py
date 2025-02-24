f196d93ad734a7af48addcebbeaddfe2cf95496e
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-24 22:09:57 +0500

    docString

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def is_leap(year):
     return False
 
 def days_in_month(input_year, input_month):
+#   This is doc string
+  """This function take two input and then gave the days in month"""
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   leap_year_check = is_leap(input_year)
   if leap_year_check and input_month == 2:
