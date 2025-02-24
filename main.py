a8d375a39c755630a8b468d8d0cb3eb6946de177
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-24 22:48:46 +0500

    calculator completed

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
-def is_leap(year):
-  if year % 4 == 0:
-    if year % 100 == 0:
-      if year % 400 == 0:
-        return True
-      else:
-        return False
-    else:
-      return True
-  else:
-    return False
+from art import logo
+def add(n1, n2):
+    return n1 + n2
 
-def days_in_month(input_year, input_month):
-#   This is doc string
-  """This function take two input and then gave the days in month"""
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  leap_year_check = is_leap(input_year)
-  if leap_year_check and input_month == 2:
-    days = 29
-  else:
-    days = month_days[input_month - 1]
-  return days
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
 
+def divide(n1,n2):
+    return n1 / n2
 
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
 
-  
-  
-#🚨 Do NOT change any of the code below 
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
+    }
+def calculator():
+    print(logo)
+    end_of_loop = False
+    num1 = float(input("what is first number "))
+    while not end_of_loop:
+        
+        num2 = float(input("what is second number "))
 
+        for symbol in operations:
+            print(symbol)
 
+        operation_symbol = input("Please pick the operation above mention ")
 
+        function = operations[operation_symbol]
+        answer = function(n1=num1, n2= num2)
 
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        result = input(f"Did you want to continue with the {answer} 'y' or start new calculation 'no'")
+        if result ==  "y":
+            num1 = answer
+        else:
+            end_of_loop = True
+            calculator()
+        
 
-
-
-
-
-
-
-
+calculator()
