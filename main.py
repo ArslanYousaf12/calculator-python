375d72dc9b925d77a45a7b253e04ae13df534523
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-24 22:04:29 +0500

    first challenege done

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False
     else:
-      print("Leap year.")
+      return True
   else:
-    print("Not leap year.")
+    return False
 
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+def days_in_month(input_year, input_month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  leap_year_check = is_leap(input_year)
+  if leap_year_check and input_month == 2:
+    days = 29
+  else:
+    days = month_days[input_month - 1]
+  return days
+
+
+
+
   
   
 #🚨 Do NOT change any of the code below 
